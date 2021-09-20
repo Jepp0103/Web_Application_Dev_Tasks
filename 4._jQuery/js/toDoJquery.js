6b376cd2110a0c04627408c83df203504fdd54f2
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-09-20 18:23:52 +0200

    working on to do list in jquery

--- 4._jQuery/js/toDoJquery.js
+++ 4._jQuery/js/toDoJquery.js
@@ -1,8 +1,69 @@
-let toDoTasks = [];
-
-console.log("loading??")
-
 $(document).ready(function () {
+    addDialogs();
+    closeDialogs();
+    addTask();
+});
+
+function addTask() {
+    $("#addBtn").click(function () {
+        let addText = $("#addText");
+        let idNumber = 0;
+        let taskId = "task" + idNumber;
+
+        console.log("taskId", taskId)
+        $("#tasks > table").append(`
+            <tr>              
+                <td id=${taskId} class="toDoTasks">
+                <img class="trashImg" src="../img/trashbin.png">
+                <p>${addText.val()}</p>
+                </td>
+                <td id=${taskId} class="ongoingTasks"></td>
+                <td id=${taskId} class="doneTasks"></td>
+            </tr>
+        `);
+
+        idNumber++;
+
+        //Closing and clearing input text in dialog
+        $('#addDialog').dialog('close');
+        $(".toDoTasks").css("background-color", "#ffa07a");
+        addText.val("");
+
+        $(".ongoingTasks").css("width", "100%");
+
+
+        $(".toDoTasks").draggable({
+            cursor: "move",
+            snap: ".ongoingTasks, .doneTasks",
+            stop: function () {
+
+            }
+        });
+
+        $(".ongoingTasks").droppable({
+            drop: function (event, ui) {
+                console.log("droppoing???")
+                if ($(this).attr("id") == ui.attr("id")) {
+                    console.log("dropped elem")
+                }
+            }
+        });
+
+        /*Calling delete method now when elements have been added*/
+        deleteTask();
+    });
+}
+
+function deleteTask() {
+    $(".trashImg").click(function () {
+        $(this).parents(".toDoTasks").remove();
+    });
+}
+
+
+
+function addDialogs() {
+    //Help dialog
     $('#helpDialog').dialog({
         autoOpen: false
     });
@@ -15,7 +76,18 @@ $(document).ready(function () {
         autoOpen: false
     });
 
-    $('#addBtn').click(function () {
+    //Add dialog
+    $('#addTaskBtn').click(function () {
         $('#addDialog').dialog('open');
     });
-});
+}
+
+function closeDialogs() {
+    $("#cancelBtn").click(function () {
+        $("#addDialog").dialog("close");
+    })
+
+    $("#closeBtn").click(function () {
+        $("#helpDialog").dialog("close");
+    })
+}
